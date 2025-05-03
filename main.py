411f662f2522da02dc4b9117615d05ed7718d8c6
Henry Khuong <135005371+HenryYT13@users.noreply.github.com>
2025-05-03 10:25:08 +0700

    commit 1

---
+++ main.py
@@ -0,0 +1,11 @@
+# # a = "MindX "
+# # print(len(a))
+# # print(a[0])
+# A = ["a", "b", "c", "d", "e"]
+# A[0] = "f"
+
+# B = "abcde"
+# B[0] = "f"
+A = "MindX Technology"
+for i in range(len(A)):
+    print(A[i])
